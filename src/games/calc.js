74c2e319c5dbfde17281ed217412731b214568c2
LDR-0 <dlezhayko@gmail.com>
2024-05-03 14:15:06 +0200

    remake 1

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,28 +1,30 @@
 import gameLogic from '../gameLogic.js';
 
+const generateExpression = () => {
+  const number = Math.floor(Math.random() * 100);
+  const symbols = ['+', '-', '*'];
+  const randomIndex = Math.floor(Math.random() * symbols.length);
+  const randomSymbol = symbols[randomIndex];
+  let answer = 0;
+  switch (randomSymbol) {
+    case '+':
+      answer = number + number;
+      break;
+    case '-':
+      answer = number - number;
+      break;
+    case '*':
+      answer = number * number;
+      break;
+    default:
+      answer = 1;
+  }
+  const question = `${number} ${randomSymbol} ${number}`;
+  return [question, answer.toString()];
+};
+
 export default () => {
   const rules = 'What is the result of the expression?';
-  const rulesFunction = () => {
-    const number = Math.floor(Math.random() * 100);
-    const simbols = ['+', '-', '*'];
-    const randomIndex = Math.floor(Math.random() * (simbols.length - 1));
-    const randomSimbol = simbols[randomIndex];
-    let answer = 0;
-    switch (randomIndex) {
-      case 0:
-        answer = number + number;
-        break;
-      case 1:
-        answer = number - number;
-        break;
-      case 2:
-        answer = number * number;
-        break;
-      default:
-        answer = 1;
-    }
-    const question = `${number} ${randomSimbol} ${number}`;
-    return [question, answer.toString()];
-  };
+  const rulesFunction = generateExpression;
   gameLogic(rules, rulesFunction);
 };
